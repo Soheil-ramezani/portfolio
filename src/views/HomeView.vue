36fa3dcdac29b87bfc7d0d653b926785ee112a57
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-01-01 17:20:17 +0330

    AboutMe Block added
    Photo & Text need to change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
 import HeaderView from "@/components/Header/Header.vue"
+import AboutMe from "@/components/AboutMe/AboutMe.vue"
+
 </script>
 
 <template>
  <HeaderView/>
+ <main class="pt-24 flex flex-row  justify-center">
+    <AboutMe/>
+ </main>
 </template>
 
 <script>
