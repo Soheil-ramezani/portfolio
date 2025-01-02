f40b687c3bad7e54ec63840b4b7a57837ae0550e
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-01-02 11:33:47 +0330

    AboutMe updated
    AboutMeText get topPadding

--- src/components/AboutMe/AboutMe.vue
+++ src/components/AboutMe/AboutMe.vue
@@ -5,7 +5,7 @@
       <img src="../../assets/images/resume.png" alt="" />
     </div>
     <!-- AboutMeText -->
-     <div class="AboutMeText flex flex-col mr-20">
+     <div class="AboutMeText flex flex-col pt-16 mr-20">
       <h1>سهیل رمضانی</h1>
       <p class="AboutMeText__p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt numquam a animi, fugiat, aperiam blanditiis pariatur in neque nemo nihil iusto, reiciendis nam tempore illum dignissimos ex. Autem, architecto provident tenetur labore doloribus tempora mollitia? Est consequuntur vel dignissimos ex aliquam et numquam veniam, temporibus porro voluptate blanditiis eos dolor obcaecati suscipit, architecto reprehenderit fugit eligendi, totam veritatis quod! Maxime quod, inventore voluptatem asperiores velit eaque neque magni vitae, tenetur nihil quae repellendus aliquid cum totam officiis, unde sint eum cumque id autem omnis? Doloremque officia aliquid cumque soluta ea itaque totam dolorum, perspiciatis quaerat provident eaque fugiat accusamus velit?
