ce6787485411195d824a372ff661c51d423ef56d
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-01-06 16:36:56 +0330

    skill List item added

--- src/components/MySkills/MySkills.vue
+++ src/components/MySkills/MySkills.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="MySkills w-[100%] flex flex-col justify-center items-center">
+  <div class="MySkills w-[100%] flex flex-col justify-center items-center mb-10">
     <div class="SkillsTitle w-[100%] flex flex-col justify-center items-center">
       <h1 class="SkillsTitle__h1 text-center mb-5">مهارت های من</h1>
       <hr class="SkillsTitle__hr mb-5" />
     </div>
     <!-- Swiper Carousel -->
-    <div class="skills-LogoCarousel w-[80%] flex flex-row">
+    <div class="skill-List w-[100%] flex justify-center align-middle  ">
+      <div class="skill-List-item w-[50%] flex flex-row-reverse justify-center align-middle">
+        <div class="item-Logo">
+        <img src="/src/assets/Logos/Bootstrap-dark.jpg" class="item-Logo__img" alt="Bootstrap-dark">
+      </div>
+      <!--  -->
+      <div class="item-text text-wrap text-center inline-block p-5">
+        <p class="item-text__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempora quas veniam in ex, necessitatibus sint aut nisi officia est nulla nemo tempore hic dignissimos, aliquid omnis corrupti blanditiis eligendi possimus! Excepturi ipsam accusantium maiores ducimus illum! Sint officiis, dolore voluptatibus, non, totam voluptatem delectus adipisci consectetur veniam magnam cupiditate?</p>
 
+      </div>
+      </div>
+      
       </div>
   </div>
 </template>
@@ -16,21 +26,70 @@ export default {
   name: "MySkills",
   data() {
     return {
-      logos: [
+     DarkLogos: [
         {
-          src: "",
-          alt: "Logo",
+          src: "@/src/assets/Logos/vue-js-Dark.png",
+          alt: "Vue-Logo",
         },
         {
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqUWfSb6lv-_qrWulMbAgCt1h2d-kvl7yOUQ&s",
-          alt: "Logo",
+          src: "@/src/assets/Logos/JavaScript-logo.png",
+          alt: "js-Logo",
         },
         {
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuFK64QcwedBq6nTkXnnGIA_GIXVjzX_Ci3g&s",
-          alt: "Logo",
+          src: "@/src/assets/Logos/JQuery-dark.jpg",
+          alt: "JQuery-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Bootstrap-dark.jpg",
+          alt: "Bootstrap-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Tailwind-dark.jpg",
+          alt: "Tailwind-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Sass_logo-dark.png",
+          alt: "Sass-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Docker-Dark.jpg",
+          alt: "Docker-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/git-dark.jpg",
+          alt: "git-Logo",
+        },
+      ],
+     LightLogos: [
+        {
+          src: "@/src/assets/Logos/vue-js-light.png",
+          alt: "Vue-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/JavaScript-logo.png",
+          alt: "js-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/JQuery-light.jpg",
+          alt: "JQuery-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Tailwind-light.jpg",
+          alt: "Tailwind-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Sass_logo-light.png",
+          alt: "Sass-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/Docker-Light.jpg",
+          alt: "Docker-Logo",
+        },
+        {
+          src: "@/src/assets/Logos/git-light.jpg",
+          alt: "git-Logo",
         },
 
-        // Add more logos as needed
       ],
     };
   },
