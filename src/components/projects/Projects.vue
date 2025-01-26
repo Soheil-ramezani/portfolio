5753efc45ef9e8e1bcac8c59ba7a849cfcf25102
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-01-26 17:05:52 +0330

    Project Modal added

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="Projects mt-10  flex flex-col justify-center items-center">
-    <div class="Projects-title mb-10 self-end pr-[5%]">
+    <div class="Projects-title mb-10 self-center">
         <h1 class="Projects-title__h1">آثار من: سفرهای خلاقانه</h1>
         
     </div>
+    <!-- Projects List -->
     <div class="projects-list mb-60">
-        <div class="project-list-item flex flex-col items-center bg-gray-700 pt-5 pl-3" v-for="project in MyProjects" :key="project.id">
+        <div class="project-list-item flex flex-col items-center bg-gray-700 pt-5 pl-3" v-for="project in MyProjects" :key="project.id" >
             <div class="item__img w-[90%] ">
                 <img :src="project.img" :alt="project.name">
             </div>
@@ -13,8 +14,23 @@
             <h1 class="niconne-regular">
             {{ project.name }}
           </h1>
+          <button class="item__btn text-center w-[90%]" @click="OpenModal(project.img , project.name ,project.name ,project.area , project.pages, project.techs , project.address)">مشاهده جزئیات</button>
           </div> 
         </div>
+    </div>
+ <!-- Project-Modal -->
+ <div ref="ProjectModal" class="Project-modal">
+      <!-- Modal content -->
+      <div class="modal-content flex flex-col justify-center align-middle">
+        <span class="close text-end" @click="CloseModal()">&times;</span>
+        <img ref="modal__img" class="modal__img self-center mb-4" src="" alt="" />
+        <h1  class="modal__h1 mb-3 text-center dir--rtl niconne-regular" ref="modal__h1"></h1>
+        <h3 class="dir--rtl text-center" ref="modal__h3_title"></h3>
+        <h3 class="dir--rtl text-center" ref="modal__h3_area"></h3>
+        <h3 class="dir--rtl text-center" ref="modal__h3_pages"></h3>
+        <h3 class="dir--rtl text-center" ref="modal__h3_techs"></h3>
+        <button class="modal__btn mt-5"><a ref="modal__btn_a" class="modal__btn_a dir--rtl" href="" type="blank"></a></button>
+      </div>
     </div>
   </div>
 </template>
@@ -25,11 +41,35 @@ name:"MyProjects",
 data(){
   return{
     MyProjects:[
-      {id:1 , name:"Golden Movie" , img:"/src/assets/images/GoldenMovie.png" , address:"https://golden-movie-tau.vercel.app/",info:"GoldenMovieیک سایت برای دانلود فیلم و سریال است  و شامل صفحات زیر میشود:" , techs:["JavaScript", "Vite"," Vue js", "Sass","Bootstrap"],pages:["Login","خانه","توضیحات فیلم"] }
+      {id:1 , name:"Golden Movie" , img:"/src/assets/images/GoldenMovie.png" , address:"https://golden-movie-tau.vercel.app/",pages:["LogIn","خانه","توضیحات فیلم"],area:"یک سایت برای دانلود فیلم و سریال است  " , techs:"JavaScript ،Vite ، Vue js ، Sass ، Bootstrap"  }
     ]
 
   }
-}
+},
+methods: {
+    OpenModal(imgSrc,imgAlt,title,area,pages, techs,address) {
+      this.$refs.modal__img.src = imgSrc;
+      this.$refs.modal__img.alt = imgAlt;
+      this.$refs.modal__h1.innerHTML = title;
+      this.$refs.modal__h3_area.innerHTML = "حوزه:"+'</br>'+area;
+      this.$refs.modal__h3_pages.innerHTML = "صفحات:"+'</br>'+pages;
+      this.$refs.modal__h3_techs.innerHTML = "زبان و فریمورک ها:"+'</br>'+techs;
+      this.$refs.modal__btn_a.href=address;
+      this.$refs.modal__btn_a.innerHTML= title+"رفتن به سایت";
+      this.$refs.ProjectModal.style.display = "block";
+    },
+    CloseModal() {
+      this.$refs.ProjectModal.style.display = "none";
+    },
+    handleClickOutside(event) {
+      if (event.target === this.$refs.ProjectModal) {
+        this.CloseModal();
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener("click", this.handleClickOutside);
+  },
 }
 </script>
 
